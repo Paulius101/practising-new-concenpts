4f8f154b32c944daae9d0cf8231d8e47a8bc43ae
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2022-01-16 18:02:43 +0200

    reactive form custom async validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameEditorComponent } from './components/name-editor/name-editor.component';
 import { ProfileEditorComponent } from './components/profile-editor/profile-editor.component';
+import { HeroFormReactiveComponent } from './components/reactive/hero-form-reactive.component';
+import { ForbiddenValidatorDirective } from './components/shared/forbidden-name.directive';
+import { IdentityRevealedValidatorDirective } from './components/shared/identity-revealed.directive';
+import { UniqueAlterEgoValidatorDirective } from './components/shared/alter-ego.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NameEditorComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+     ForbiddenValidatorDirective,
+    IdentityRevealedValidatorDirective,
+    UniqueAlterEgoValidatorDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
